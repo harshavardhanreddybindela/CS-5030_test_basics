b9e8a845f15517b029e582f23c7c3b0af0f44b68
harshavardhanreddybindela <113057483+harshavardhanreddybindela@users.noreply.github.com>
2022-11-07 17:16:50 -0600

    psd_assignment_testcases

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -19,9 +19,27 @@ describe('todo test suite', () => {
         expect(todo_service.get_todos().todo.length).toEqual(3);
     });
 
-    
+    //add test case
+    test("add_todo", () =>{
+        todo = {
+            "id":4,
+            "title": "T3",
+            "description": "D2",
+            "done": false
+        }
+        todo_service.add_todo(todo);
+        expect(todo_service.get_todos().length).toEqual(4);
+    })
 
-    // Write all your test cases here that corresponds to software requirements
+    //update test case
+    test("update_todo", () => {
+        todo_service.update_todo(1,'T2','This is new description',false);
+        expect(todo_service.get_todos().length).toEqual(4);
+    });
 
-
+    //delete test case
+    test("delete_todo", () => {
+        todo_service.delete_todo(3);
+        expect(todo_service.get_todos().length).toEqual(3);
+    });
 });
